400b54831cc1a2b7da7bd5e4155721702bb996f8
Saparbek28 <46023456+Saparbek28@users.noreply.github.com>
2019-03-28 02:48:37 +0600

    adding file

---
+++ Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    public enum Sex
+    {
+        FEMALE,
+        MALE
+    }
+    public enum EduType
+    {
+        TYPE1,
+        TYPE2
+    }
+
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string GroupName { get; set; }
+        public double AvgValue { get; set; }
+        public int ParentsSalary { get; set; }
+        public Sex Sex { get; set; }
+        public EduType EduType { get; set; }
+    }
+}
